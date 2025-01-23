4577a6a2e1ebc80d51a1b50a1a86912cab8605c3
Mmoussier <Girt@b18>
2025-01-23 18:29:40 +0100

    Désérialisation et sérialisation avec Jackson / avec objets hérités

--- src/main/java/com/morgann/xml/jackson/ElementDetails.java
+++ src/main/java/com/morgann/xml/jackson/ElementDetails.java
@@ -1,18 +1,22 @@
 package com.morgann.xml.jackson;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonSubTypes;
 import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 
+import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 
 // @JsonTypeInfo indique sur quelle propriété on se base pour déterminer le type d'objet à créer lors de la désérialisation
-@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "elementType")
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, include = JsonTypeInfo.As.EXISTING_PROPERTY, property = "elementType", visible = true)
 // Si elementType="LABEL" : on crée un objet LabelElementDetails
 // Si elementType="SHEET" : on crée un objet ElementDetails
 @JsonSubTypes({@JsonSubTypes.Type(value = LabelElementDetails.class, name = "LABEL"),
         @JsonSubTypes.Type(value = ElementDetails.class, name = "SHEET")})
 @XmlRootElement(name = "elementDetails")
 public class ElementDetails {
+    @XmlElement
     private String elementType;
     private double width;
     private double height;
